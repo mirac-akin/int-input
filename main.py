50386307f1665f21942c014544f196b84c207a67
mirac-akin <123837687+mirac-akin@users.noreply.github.com>
2023-02-01 12:04:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# <input> etiketi kullanıcının veri girişi yapabileceği giriş alanı tanımlar 
+#input örnekleri
+isim= input ("isminiz")
+Kackez = input("Kaç kez tekrar edilsin?")
+print(İsim * int(Kackez))
+#<int>Hesaplamalarda, işlemlerde, tanımladığınız enum yapılarında, tam sayı değer kullanmanız gereken her yerde kullanabilirsiniz
+#int örnek 
+#int abc
+a=9
+b=12
+c=13
+a+b+c
