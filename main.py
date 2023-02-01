852efcaa90e65dd8b97ebcf663396d841c9ea00e
mirac-akin <123837687+mirac-akin@users.noreply.github.com>
2023-02-01 12:42:26 +0000

    I learned int and input in today's lesson

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 # <input> etiketi kullanıcının veri girişi yapabileceği giriş alanı tanımlar 
 #input örnekleri
 isim= input ("isminiz")
-Kackez = input("Kaç kez tekrar edilsin?")
-print(İsim * int(Kackez))
+Kackez = input("soy adınız")
 #<int>Hesaplamalarda, işlemlerde, tanımladığınız enum yapılarında, tam sayı değer kullanmanız gereken her yerde kullanabilirsiniz
 #int örnek 
 #int abc
-a=9
-b=12
-c=13
-a+b+c
+print_int=234
+print_int=163
---
+++ notes.py
@@ -0,0 +1,44 @@
+# kare ile tek satırlı yorum yazabiliyoruz
+
+"""
+üç tırnaklada çok satırlı yorum yazabiliyoruz
+"""
+
+"""
+**DEĞİŞKENLERİN İSMİ VE DEĞERİ**
+eşitliğin solu -> değişkenin ismi = eşitliğin sağı -> değişkenin değeri
+kilo = 48
+"""
+
+"""
+**DEĞİŞKEN İSİMLENDİRME HATALARI**
+1)my var = "John" -> yanlış çünkü değişkenin isminde boşluk olamaz    
+2)2myvar = "John" -> yanlış çünkü değişkenin ismi sayı ile başlayamaz 
+3)my-var = "John" -> yanlış çünkü degişkenin isminde kısa çizgi olamaz
+4)print  = "john" -> çünkü programlama dilinin özel kelimeleri kullanılamaz
+"""
+
+"""
+**ÇOK KELİMELİ İSİMLENDİRME ŞEKİLLERİ**
+camelCase  -> ilk kelimenin baş harfi küçük, sonraki kelimelerin baş harfi büyük
+PascalCase -> her kelimenin baş harfi büyük
+snake_case -> kelimelerin arasında alt tire var #fonksiyonlarda bu kullanılıyor!
+"""
+
+
+"""
+**DEĞİŞKEN ÇEŞİTLERİ**
+tam sayı->integer->int->%d->13
+küsüratlı sayı->float->float->%f->2.5
+yazı->string->str->%s->"selam" # değeri tırnak içinde yazılır!
+"""
+
+"""
+**DEĞİŞKEN YAZMA ÖRNEK**
+A) Iq puanını belirten rastgele tam sayı değerli bir değişken tanımlayın ve türünü yorum olarak belirtin.
+IqScore=12 #int
+B) Boyu belirten rastgele ondalık(küsüratlı) değerli bir değişken tanımlayın ve türünü yorum olarak belirtin.
+height=1.60 #float
+C) Bir öğrencinin ismini belirten bir rastgele değerli bir değişken tanımayın ve türünü yorum olarak belirtin.
+StudentName="Mirac" #string
+"""
