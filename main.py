2b55ad5e112d926482afae0cfcf99befeddaa421
mirac-akin <123837687+mirac-akin@users.noreply.github.com>
2023-02-08 19:05:19 +0300

    Update main.py
    int

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # <input> etiketi kullanıcının veri girişi yapabileceği giriş alanı tanımlar 
 #input örnekleri
 isim= input ("isminiz")
-Kackez = input("soy adınız")
+Kackez = input("soyadınız")
 #<int>Hesaplamalarda, işlemlerde, tanımladığınız enum yapılarında, tam sayı değer kullanmanız gereken her yerde kullanabilirsiniz
 #int örnek 
 #int abc
-print_int=234
-print_int=163
+yaş= int ("yaşınızıgriniz
+
